abadfd2546fd4a270f6b3a02343984965d64976e
gelman-usc <igelman@usc.edu>
2020-03-09 19:03:41 -0700

    stream_test

---
+++ pipe/stream_test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+
+int main()
+{
+	string s;
+
+	while (cin >> s)
+	{
+		cout << s << endl;
+	}
+
+
+	return 0;
+}
