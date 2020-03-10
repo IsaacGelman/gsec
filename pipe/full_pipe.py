b610660b3597265f3ca72dba1ff976c24889c8ea
Nicolas Perez <nicolagp@usc.edu>
2020-03-10 12:38:35 -0700

    added counting to full pipeline

--- pipe/full_pipe.py
+++ pipe/full_pipe.py
@@ -7,17 +7,19 @@ import sys
 
 
 """
-Usage: python3 aseqc.py 1 2 3 4
+Usage: python3 full_pipe.py 1 2 3 4 5 6
 Arguments:
 1) positive strategy (string)
 2) positive organism (string)
 3) negative strategy (string)
 4) negative organism (stirng)
+5) max kmer to count (int)
+6) limit of reads for each srr (int)
 
 """
 def main(argv):
 	# Check arguments
-	if len(argv[1:]) != 4:
+	if len(argv[1:]) != 6:
 		print('Usage: ')
 		print('python3 aseqc.py pos_strat pos_org neg_strat neg_org')
 		return 1
@@ -26,7 +28,9 @@ def main(argv):
 	# Get positive and negative strings
 	pos_strat, pos_org = argv[1], argv[2]
 	neg_strat, neg_org = argv[3], argv[4]
-	
+	k = argv[5]
+	limit = argv[6]	
+
 	# Queries
 	pos_query = 'esearch -db sra -query "{}[strategy] AND {}[organism]" | efetch -db sra -format docsum > {}'.format(pos_strat, pos_org, 'pos.xml')
 	
@@ -40,16 +44,28 @@ def main(argv):
 
 	# delete temp srr files
 	os.system('rm pos.xml neg.xml')
-	
-	# download files
-	print("Downloading positive...")
+
+	# count srrs
+	print("Counting positives...")	
 	for srr in pos_srrs:
-		os.system('srapath {} | curl'.format(srr))
-	print("Downloading negative...")
+		count(k, limit, srr)
+	print("Counting negatives...")
 	for srr in neg_srrs:
-		os.system('srapath {} | curl'.format(srr))
+		count(k, limit, srr)
 
 	return 0
+
+"""
+k (int): max kmer to count
+limit (int): limit number of reads to count for given file
+srr (str): srr id for read
+"""
+def count(k, limit, srr):
+	# shell commands to run
+	fastq = "fastq-dump --skip-technical --split-spot -Z {}"	
+	count = "./stream_kmers {} {} > out.txt".format(str(k), str(limit))
+	full = fastq + " | " + count
+	subprocess.call(full, shell=True)
 
 
 """
