8aa2a0822fa6ec2eb7f28877472b82441a15b62b
Nicolas Perez <nicolagp@usc.edu>
2020-03-14 12:35:29 -0300

    changed out directory for count files

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -17,12 +17,13 @@ def main(argv):
     4) negative organism (stirng)
     5) max kmer to count (int)
     6) limit of reads for each srr (int)
-
+    7) directory to save files
     """
     # Check arguments
-    if len(argv[1:]) != 6:
+    if len(argv[1:]) != 7:
         print('Usage: ')
-        print('python3 aseqc.py pos_strat pos_org neg_strat neg_org')
+        print('python3 preprocessing.py pos_strat pos_org neg_strat neg_org \
+        max_k limit_reads out_dir')
         return 1
 
 
@@ -31,6 +32,7 @@ def main(argv):
     neg_strat, neg_org = argv[3], argv[4]
     k = argv[5]
     limit = argv[6]
+    out = argv[7]
 
     # Queries
     pos_query = 'esearch -db sra -query "{}[strategy] AND {}[organism]" | \
@@ -43,6 +45,7 @@ def main(argv):
 
     # get srrs
     pos_srrs = parse_xml('pos.xml', 5)
+
     neg_srrs = parse_xml('neg.xml', 5)
 
     # delete temp srr files
@@ -51,15 +54,15 @@ def main(argv):
     # count srrs
     print("Counting positives...")
     for srr in pos_srrs:
-        count(k, limit, srr)
+        count(k, limit, srr, out+'/positive')
     print("Counting negatives...")
     for srr in neg_srrs:
-        count(k, limit, srr)
+        count(k, limit, srr, out+'/negative')
 
     return 0
 
 
-def count(k, limit, srr):
+def count(k, limit, srr, out):
     """
     k (int): max kmer to count
     limit (int): limit number of reads to count for given file
@@ -71,8 +74,11 @@ def count(k, limit, srr):
         subprocess.call('g++ stream_kmers.cpp -o stream_kmers', shell=True)
 
     # shell commands to run
+    filename = out+'/'+srr+'.txt'
     fastq = "fastq-dump --skip-technical --split-spot -Z {}".format(srr)
-    count = "./stream_kmers {} {} > out.txt".format(str(k), str(limit))
+    count = "./stream_kmers {} {} > {}".format(str(k),
+                                               str(limit),
+                                               str(filename))
     full = fastq + " | " + count
     subprocess.call(full, shell=True)
 
@@ -84,6 +90,7 @@ def parse_xml(filename, n):
     n (int): number of srrs to fetch, -1 for all
     returns (list[str]): list of SRRs to download
     """
+
     tree = ET.parse(filename)
     root = tree.getroot()
 
