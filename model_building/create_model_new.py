2127f953866ff2fcc9432afb0465ecb0d9cd70b8
Tomas Angelini <tangelin@usc.edu>
2020-03-18 19:02:54 -0300

    addded functionality for different lists of kmer counts when checking text files. added efficiency_check function to calculate average runtime of dataframe generation

--- model_building/create_model_new.py
+++ model_building/create_model_new.py
@@ -43,8 +43,20 @@ def dir_check():
     print("model directory: ", model_dir)
     print("data directory: ", data_dir)
 
+def calculate_dimension(kmer_list):
+    '''
+    Function takes list of kmers chosen for the dataset
+    and calculates how many rows the corresponding txt files
+    should have when processing
+    '''
+    total_count = 0
+    for i in kmer_list:
+        total_count += pow(4,i)
 
-def file_shell(df,proj,fname,ind):
+    return total_count
+
+
+def file_shell(df,proj,fname, kmer_count, ind):
     """
     Append experiment of name "fname" to dataframe
     """
@@ -53,20 +65,20 @@ def file_shell(df,proj,fname,ind):
     '''
     If number of rows in txt file doesn't match number of kmers, don't append
     '''
-    if not df.empty:
-        if ((df_new.shape)[0] != (df.shape)[1]):
-            error_file = open("errors.txt", "a+")
-            error_file.write("\r%s, %s, missing_kmers" % (proj.name,
-            fname.name))
-            return df
 
-    df_new.set_index(0, inplace=True)
-    df_new = df_new.transpose()
-    df_new.index = [ind]
-    return df.append(df_new)
+    if ((df_new.shape)[0] != kmer_count):
+        error_file = open("errors.txt", "a+")
+        error_file.write("\r%s, %s, missing_kmers" % (proj.name,
+        fname.name))
+        return df
+    else:
+        df_new.set_index(0, inplace=True)
+        df_new = df_new.transpose()
+        df_new.index = [ind]
+        return df.append(df_new)
 
 
-def load_data(data_name, df):
+def load_data(data_name, df, kmer_count):
     """
     Function loads a certain type of data
     for each experiment, call file_shell
@@ -82,7 +94,7 @@ def load_data(data_name, df):
             for experiment in experiment_list.iterdir():
                 #print("Current exp: %s" % experiment)
                 if os.stat(experiment).st_size != 0:
-                    df = file_shell(df, project, experiment, 1)
+                    df = file_shell(df, project, experiment, kmer_count, 1)
                 else:
                     #print("ERROR FOUND IN FILE %s" % experiment)
                     error_file = open("errors.txt", "a+")
@@ -90,24 +102,48 @@ def load_data(data_name, df):
                     experiment.name))
     return df
 
-
-def main():
+def create_dataframe(first_data_type, second_data_type, kmer_list):
+    '''
+    Function takes inputs the two data types to create the
+    dataset from (e.g. wgs vs. rna), and returns a dataframe
+    with the kmer counts of both datatypes
+    '''
     clear_errors()
-    start_time = time.time()
     df = pd.DataFrame()
-    # dir_check()
-    #df = load_data("rna-seq", df)
-    df = load_data("wgs_human", df)
+    kmer_count = calculate_dimension(kmer_list)
+    df = load_data(first_data_type, df, kmer_count)
+    df = load_data(second_data_type, df, kmer_count)
+    return df
 
-    end_time = float(time.time() - start_time)
-    num_experiments = float(len(df.index))
-    avg = end_time / num_experiments
-    exp_per_min = 1 / avg * 60
+def efficiency_check(first_data_type, second_data_type, kmer_list, n):
+    '''
+    Function runs the dataframe creation process n times and generates
+    average runtime stats
+    '''
+    abs_avg = 0
+    abs_exp_per_min = 0
 
+    for i in range(0,n):
+        tmp_df = pd.DataFrame()
+        start_time = time.time()
+        tmp_df = create_dataframe(first_data_type, second_data_type, kmer_list)
+        end_time = float(time.time() - start_time)
+        num_experiments = float(len(tmp_df.index))
+
+        avg = end_time / num_experiments
+        exp_per_min = 1 / avg * 60
+
+        abs_avg += avg
+        abs_exp_per_min += exp_per_min
+
+    abs_avg = abs_avg / 100
+    abs_exp_per_min = abs_exp_per_min / 100
 
     print("Average loading time: %f seconds per experiment" % avg)
     print("Experiments to be processed per minute: %f" % exp_per_min)
-    print(df.shape)
+
+def main():
+    efficiency_check("rna-seq", "wgs_human", [1,2,3,4,5,6], 10)
 
 
 if __name__ == "__main__":
