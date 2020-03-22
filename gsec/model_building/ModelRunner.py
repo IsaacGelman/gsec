3bfa8a1bd73e96b1aa8a60eb72af88bd14906353
Nicolas <nicolagp@usc.edu>
2020-03-22 13:52:32 -0700

    refactor create_model

---
+++ gsec/model_building/ModelRunner.py
@@ -0,0 +1,133 @@
+import numpy as np
+import pandas as pd
+import os
+from pathlib import Path
+import pickle
+from sklearn.model_selection import train_test_split, GridSearchCV, \
+cross_val_score
+from sklearn import naive_bayes
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import roc_auc_score, accuracy_score, classification_report
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.linear_model import LinearRegression, LogisticRegressionCV, Lasso
+from sklearn import neighbors
+
+class ModelRunner():
+
+    def __init__(self, df):
+        # create splits
+        y = df.index
+        self.X_train, \
+        self.X_test, \
+        self.y_train, \
+        self.y_test = train_test_split(
+            df.reset_index(drop=True),
+            y,
+            test_size=0.25,
+            shuffle=True,
+            stratify=y
+            )
+
+        # models
+        self.models = {}
+
+    def log_reg():
+        log_reg = LogisticRegressionCV(cv=5, random_state=0)
+        log_reg.fit(self.X_train, self.y_train)
+
+        pred = log_reg.predict(self.X_test)
+
+        self.models['log_reg'] = (log_reg, accuracy_score(self.y_test, pred))
+
+    def knn():
+        base_knn = neighbors.KNeighborsClassifier()
+        parameters = {
+            'n_neighbors': [1, 2, 5, 10, 15, 25],
+            'weights': ['uniform', 'distance']
+        }
+
+        knn = GridSearchCV(base_knn, parameters, cv=3)
+        knn.fit(X_train, y_train)
+
+        pred = knn.predict(X_test)
+
+        self.models['knn'] = (knn, accuracy_score(self.y_test, pred))
+
+    def gnb():
+        base_gnb = GaussianNB()
+        parameters = {'var_smoothing': [1e-20, 
+                                        1e-15, 
+                                        1e-10, 
+                                        1e-05, 
+                                        1e01, 
+                                        1e-05]}    
+        gnb = GridSearchCV(base_gnb, parameters, cv=3)
+        gnb.fit(self.X_train, self.y_train)
+        pred = gnb.predict(self.X_test)
+        self.models['gaussnb'] = (gnb, accuracy_score(self.y_test, pred))
+
+    def rf():
+        # random forest
+        base_rf = RandomForestClassifier(n_estimators=50)
+        parameters = {
+            'bootstrap': [True],
+             'max_depth': [90, 100],
+              'n_estimators': [50, 100, 200]
+        }
+        rf = GridSearchCV(base_rf, parameters, cv=3)
+        rf.fit(self.X_train, self.y_train)
+        pred = rf.predict(self.X_test)
+        self.models['rand_forest'] = (rf, accuracy_score(self.y_test, pred))
+
+    def ensemble():
+        # get model name and trained model
+        estimators = [(item[0], item[1][0]) for item in self.models.items()]
+        eclf = VotingClassifier(estimators=estimators,voting='soft')
+
+        eclf.fit(X_train, y_train)
+
+        pred = eclf.predict(X_test)
+
+        self.models['ensemble'] = (ensemble, accuracy_score(self.y_test, pred))
+
+    def lasso():
+        # lasso
+        parameters = {'alpha': [1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2, 1, 5, 10]}
+        lassoclf = Lasso(alpha=.0005, tol=1)
+        print(cross_val_score(lassoclf, X_train, y_train, cv=3))
+        lassoclf.fit(X_train, y_train)
+
+        np.set_printoptions(threshold=np.inf)
+        coefficients = lassoclf.coef_
+        df.columns.where(coefficients != 0).dropna()
+
+        pred = lassoclf.predict(X_test)
+
+        print('Accuracy: ', accuracy_score(y_test, 1 * (pred > 0.5)))
+        print(classification_report(y_test, 1 * (pred > 0.5)))
+
+        model_scores['log-reg'] = accuracy_score(y_test, 1 * (pred > 0.5))
+        model_types['log-reg'] = lassoclf
+
+
+    def get_best_model():
+        """
+        returns the best model and max score (in this order) 
+        """
+        max_score = -1.0
+        best_model = None
+
+        for model in self.models:
+            if model[1] > max_score:
+                max_score = model[1]
+                best_model = model[0]
+
+        return best_model, max_score
+    
+    def save_model(model, file_dir):
+        """
+        model: model to save
+        file_dir: path to save model file
+        """
+        with open(file_dir, "wb") as file:
+            pickle.dump(model_types[model_type_string], file)
--- gsec/model_building/create_model.py
+++ gsec/model_building/create_model.py
@@ -1,51 +1,10 @@
-# Importing Stuff
+from .ModelRunner import ModelRunner
 
-import pandas as pd
-import numpy as nump
-import sklearn as sk
-import os
-from pathlib import Path
-import pickle as pickle
-from sklearn.model_selection import train_test_split
-from sklearn import naive_bayes
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import LogisticRegressionCV
-from sklearn import neighbors
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import Lasso
-from sklearn import linear_model
-import time
-from glob import glob
-
-#model_dir = os.path.dirname(os.path.realpath(__file__))
-
-#existing_models = glob(model_dir + '*.pkl')
-#model_filename = model_dir + 'model%d.pkl' % (len(existing_models) + 1)
-
-
-# function to normalize data
-def normalize(df, maxk):
-  start_index = 0
-  for k in range(1,maxk+1):
-    df.iloc[:,start_index:start_index+4**k] = \
-    df.iloc[:,start_index:start_index+4**k].div(
-        df.iloc[:,start_index:start_index+4**k].sum(axis=1), axis='rows')
-
-    start_index = start_index+4**k
 
 # function for creating and saving model
 def create_model(df, maxk, id):
 
-    # normalize
-    #normalize(df,maxk)
+    # dropping nulls
     df.dropna(inplace=True)
 
     # file names and paths
@@ -58,145 +17,33 @@ def create_model(df, maxk, id):
     if not os.path.exists(models_dir):
         os.mkdir(models_dir)
 
+    # create Model class
+    runner = ModelRunner(df)
 
-    # split train and test
-    y = df.index
-    X_train, X_test, y_train, y_test = train_test_split(
-        df.reset_index(drop=True), y, test_size=0.25, shuffle=True,
-                       stratify=y)
-
-    # init dictionaries
-    model_scores = {}
-    model_types = {}
-
-    # models
+    # try different models
 
     # log-reg
-    log_reg = LogisticRegressionCV(cv=5, random_state=0)
-    print(cross_val_score(log_reg, X_train, y_train, cv=3))
-    log_reg.fit(X_train, y_train)
-
-    pred = log_reg.predict(X_test)
-    scores = log_reg.predict_proba(X_test)[:, 1]
-
-    print('Accuracy: ', accuracy_score(y_test, pred))
-    print('AUROC: ', roc_auc_score(y_test, scores))
-    print(classification_report(y_test, pred))
-
-    model_scores['logistic'] = accuracy_score(y_test, pred)
-    model_types['logistic'] = log_reg
+    runner.log_reg()
 
     # knn
-    base_knn = neighbors.KNeighborsClassifier()
-    parameters = {
-        'n_neighbors': [1, 2, 5, 10, 15, 25],
-        'weights': ['uniform', 'distance']
-        }
-
-    knn = GridSearchCV(base_knn, parameters, cv=3)
-    print(cross_val_score(knn, X_train, y_train, cv=3))
-    knn.fit(X_train, y_train)
-    print('Best Hyperparameters: ', knn.best_params_, '\n')
-
-    pred = knn.predict(X_test)
-    scores = knn.predict_proba(X_test)[:, 1]
-
-    print('Accuracy: ', accuracy_score(y_test, pred))
-    print('AUROC: ', roc_auc_score(y_test, scores))
-    print(classification_report(y_test, pred))
-
-    model_scores['knn'] = accuracy_score(y_test, pred)
-    model_types['knn'] = knn
+    runner.knn()
 
     # gauss nb
-    base_gnb = GaussianNB()
-    parameters = {'var_smoothing': [1e-20, 1e-15, 1e-10, 1e-05, 1e01, 1e-05]}
-
-    gnb = GridSearchCV(base_gnb, parameters, cv=3)
-    print(cross_val_score(gnb, X_train, y_train, cv=3))
-    gnb.fit(X_train, y_train)
-    print('Best Hyperparameters: ', gnb.best_params_, '\n')
-
-    pred = gnb.predict(X_test)
-    scores = gnb.predict_proba(X_test)[:, 1]
-
-    print('Accuracy: ', accuracy_score(y_test, pred))
-    print('AUROC: ', roc_auc_score(y_test, scores))
-    print(classification_report(y_test, pred))
-
-    model_scores['gaussnb'] = accuracy_score(y_test, pred)
-    model_types['gaussnb'] = gnb
+    runner.gnb()
 
     # random forest
-    base_rf = RandomForestClassifier(n_estimators=50)
-    parameters = {
-        'bootstrap': [True],
-         'max_depth': [90, 100],
-          'n_estimators': [50, 100, 200]
-    }
-
-    rf = GridSearchCV(base_rf, parameters, cv=3)
-    print(cross_val_score(rf, X_train, y_train, cv=3))
-    rf.fit(X_train, y_train)
-
-    pred = rf.predict(X_test)
-    scores = rf.predict_proba(X_test)[:, 1]
-
-    print('Accuracy: ', accuracy_score(y_test, pred))
-    print('AUROC: ', roc_auc_score(y_test, scores))
-    print(classification_report(y_test, pred))
-
-    model_scores['forest'] = accuracy_score(y_test, pred)
-    model_types['forest'] = rf
+    runner.rf()
 
     # ensemble
-    eclf = VotingClassifier(estimators=[
-        ('gnb', gnb),
-        ('lr', log_reg),
-        ('rf', rf)],
-        voting='soft')
-
-    print(cross_val_score(eclf, X_train, y_train, cv=3))
-    eclf.fit(X_train, y_train)
-
-    pred = eclf.predict(X_test)
-    scores = eclf.predict_proba(X_test)[:, 1]
-
-    print('Accuracy: ', accuracy_score(y_test, pred))
-    print('AUROC: ', roc_auc_score(y_test, scores))
-    print(classification_report(y_test, pred))
-
-    model_scores['ensemble'] = accuracy_score(y_test, pred)
-    model_types['ensemble'] = eclf
+    runner.ensemble()
 
     # lasso
-    lasso = Lasso()
-    parameters = {'alpha': [1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2, 1, 5, 10]}
-    lassoclf = linear_model.Lasso(alpha=.0005, tol=1)
-    print(cross_val_score(lassoclf, X_train, y_train, cv=3))
-    lassoclf.fit(X_train, y_train)
-
-    nump.set_printoptions(threshold=nump.inf)
-    coefficients = lassoclf.coef_
-    df.columns.where(coefficients != 0).dropna()
-
-    pred = lassoclf.predict(X_test)
-
-    print('Accuracy: ', accuracy_score(y_test, 1 * (pred > 0.5)))
-    print(classification_report(y_test, 1 * (pred > 0.5)))
-
-    model_scores['log-reg'] = accuracy_score(y_test, 1 * (pred > 0.5))
-    model_types['log-reg'] = lassoclf
+    runner.lasso()
 
     # get best model
-    max_score = -1.0
-    for key in model_scores:
-        if model_scores[key] > max_score:
-            max_score = model_scores[key]
-            model_type_string = key
+    best, acc = runner.get_best_model()
 
     # save model
-    with open(file_dir, "wb") as file:
-        pickle.dump(model_types[model_type_string], file)
+    runner.save_model(best, file_dir)
 
     return 0
