aa7636bcdbb2db444c9ebbd96b10f64a482f4ba7
Nicolas <nicolagp@usc.edu>
2020-03-28 12:16:03 -0700

    print confusion matrix

--- gsec/model_building/ModelRunner.py
+++ gsec/model_building/ModelRunner.py
@@ -7,7 +7,8 @@ from sklearn.model_selection import train_test_split, GridSearchCV, \
 cross_val_score
 from sklearn import naive_bayes
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import roc_auc_score, accuracy_score, classification_report
+from sklearn.metrics import roc_auc_score, accuracy_score, 
+classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.linear_model import LinearRegression, LogisticRegressionCV, Lasso
 from sklearn import neighbors
@@ -31,15 +32,18 @@ class ModelRunner():
         # models
         self.models = {}
 
-    def log_reg(self):
+    def log_reg(self, print_matrix=False):
         log_reg = LogisticRegressionCV(cv=5, random_state=0)
         log_reg.fit(self.X_train, self.y_train)
 
         pred = log_reg.predict(self.X_test)
+        if print_matrix:
+            print("Confusion Matrix: ")
+            print(confusion_matrix(self.y_train, pred))
 
         self.models['log_reg'] = (log_reg, accuracy_score(self.y_test, pred))
 
-    def knn(self):
+    def knn(self, print_matrix=False):
         base_knn = neighbors.KNeighborsClassifier()
         parameters = {
             'n_neighbors': [1, 2, 5, 10, 15, 25],
@@ -51,9 +55,13 @@ class ModelRunner():
 
         pred = knn.predict(self.X_test)
 
+        if print_matrix:
+            print("Confusion Matrix: ")
+            print(confusion_matrix(self.y_train, pred))
+
         self.models['knn'] = (knn, accuracy_score(self.y_test, pred))
 
-    def gnb(self):
+    def gnb(self, print_matrix=False):
         base_gnb = GaussianNB()
         parameters = {'var_smoothing': [1e-20, 
                                         1e-15, 
@@ -64,9 +72,13 @@ class ModelRunner():
         gnb = GridSearchCV(base_gnb, parameters, cv=3)
         gnb.fit(self.X_train, self.y_train)
         pred = gnb.predict(self.X_test)
+
+        if print_matrix:
+            print("Confusion Matrix: ")
+            print(confusion_matrix(self.y_train, pred))
         self.models['gaussnb'] = (gnb, accuracy_score(self.y_test, pred))
 
-    def rf(self):
+    def rf(self, print_matrix=False):
         # random forest
         base_rf = RandomForestClassifier(n_estimators=50)
         parameters = {
@@ -77,9 +89,13 @@ class ModelRunner():
         rf = GridSearchCV(base_rf, parameters, cv=3)
         rf.fit(self.X_train, self.y_train)
         pred = rf.predict(self.X_test)
+
+        if print_matrix:
+            print("Confusion Matrix: ")
+            print(confusion_matrix(self.y_train, pred))
         self.models['rand_forest'] = (rf, accuracy_score(self.y_test, pred))
 
-    def ensemble(self):
+    def ensemble(self, print_matrix=False):
         # get model name and trained model
         estimators = [(item[0], item[1][0]) for item in self.models.items()]
         eclf = VotingClassifier(estimators=estimators,voting='soft')
@@ -88,9 +104,13 @@ class ModelRunner():
 
         pred = eclf.predict(self.X_test)
 
+        if print_matrix:
+            print("Confusion Matrix: ")
+            print(confusion_matrix(self.y_train, pred))
+
         self.models['ensemble'] = (eclf, accuracy_score(self.y_test, pred))
 
-    def lasso(self):
+    def lasso(self, print_matrix=False):
         # lasso
         parameters = {'alpha': [1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2, 1, 5, 10]}
         lassoclf = Lasso(alpha=.0005, tol=1)
@@ -102,6 +122,10 @@ class ModelRunner():
         df.columns.where(coefficients != 0).dropna()
 
         pred = lassoclf.predict(self.X_test)
+
+        if print_matrix:
+            print("Confusion Matrix: ")
+            print(confusion_matrix(self.y_train, pred))
 
         # print('Accuracy: ', accuracy_score(y_test, 1 * (pred > 0.5)))
         # print(classification_report(y_test, 1 * (pred > 0.5)))
